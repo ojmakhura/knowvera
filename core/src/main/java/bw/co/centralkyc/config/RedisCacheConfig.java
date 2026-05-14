35cbe164beae3206d3c5dc1e40f4b4b9c7424265
Onalenna J. Makhura <ojmakhura@roguesystems.co.bw>
2026-05-14 19:55:54 +0200

    cache configs

--- core/src/main/java/bw/co/centralkyc/config/RedisCacheConfig.java
+++ core/src/main/java/bw/co/centralkyc/config/RedisCacheConfig.java
@@ -1,8 +1,11 @@
 package bw.co.centralkyc.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisCacheConfig {
+    
     
 }
